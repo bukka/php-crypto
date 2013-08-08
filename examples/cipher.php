60947a34d60ffff898952c47660bae6ddd13f314
Jakub Zelenka <jakub.zelenka@gmail.com>
2013-08-08 18:53:40 +0100

    Improve example script for cipher testing

--- examples/cipher.php
+++ examples/cipher.php
@@ -4,7 +4,19 @@ try {
 	$md = new MD('md5');
 
 	$cipher = new Cipher('aes-256-ctr');
-	$cipher->encryptInit("1234");
+	var_dump($cipher->getAlgorithm());
+	$key = str_repeat('x', 32);
+	$iv = str_repeat('i', 16);
+	$cipher->encryptInit($key, $iv);
+
+	$in = "jakub";
+	$out = $cipher->encryptUpdate($in);
+	$out_final = $cipher->encryptFinal();
+	$out .= $out_final;
+	var_dump(strlen($out_final));
+	var_dump(base64_encode($out));
+	var_dump(openssl_encrypt($in, $cipher->getAlgorithm(), $key, 0, $iv));
+	var_dump(openssl_decrypt($out, $cipher->getAlgorithm(), $key, OPENSSL_RAW_DATA, $iv));
 }
 catch (AlgorithmException $e) {
 	echo $e->getMessage() . PHP_EOL;
