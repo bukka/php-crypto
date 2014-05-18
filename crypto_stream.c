e1fd2f8b74a1ac442b97ab8dca47f58be0dd49b4
Jakub Zelenka <bukka@php.net>
2014-05-18 16:29:19 +0100

    Supress warning for unused BIO_flush return value

--- crypto_stream.c
+++ crypto_stream.c
@@ -83,7 +83,7 @@ static int php_crypto_stream_close(php_stream *stream, int close_handle TSRMLS_D
 static int php_crypto_stream_flush(php_stream *stream TSRMLS_DC)
 {
 	php_crypto_stream_data *data = (php_crypto_stream_data *) stream->abstract;
-	BIO_flush(data->bio);
+	(void) BIO_flush(data->bio);
 	return 0;
 }
 /* }}} */
