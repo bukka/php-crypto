64a14587d0c0268befb9c6e49cc6e478dd468752
Jakub Zelenka <bukka@php.net>
2014-07-27 15:49:02 +0100

    Rename and tidy up the example for stream cipher with cbc mode

--- examples/stream_cipher_cbc.php
+++ examples/stream_cipher_cbc.php
@@ -1,13 +1,20 @@
 <?php
 
-$filename = (dirname( __FILE__) . "/stream_file_plain_write.tmp");
+// INIT
 
+// temporary file
+$filename = tempnam(sys_get_temp_dir(), 'php_crypto_');
+// use AES with CBC mode
 $algorithm = 'aes-256-cbc';
 $key = str_repeat('x', 32);
 $iv = str_repeat('i', 16);
 $data = str_repeat('a', 16);
 
-// WRITE
+
+// ---------------------------
+// WRITE to the encrypted file
+
+// create a stream context with cipher filter
 $context_write = stream_context_create(array(
 	'crypto' => array(
 		'filters' => array(
@@ -27,11 +34,15 @@ if (!$stream_write) {
 }
 fwrite($stream_write, $data);
 fflush($stream_write);
-echo "FILE encrypted (base64):" . PHP_EOL;
+echo "FILE '$filename' encrypted (base64):" . PHP_EOL;
 echo base64_encode(file_get_contents($filename));
 echo PHP_EOL;
 
-// READ
+
+// ---------------------------
+// READ encrypted file
+
+// create a stream context with cipher filter
 $context_read = stream_context_create(array(
 	'crypto' => array(
 		'filters' => array(
@@ -45,7 +56,7 @@ $context_read = stream_context_create(array(
 		)
 	),
 ));
-echo "FILE decrypted (plain):" . PHP_EOL;
+echo "FILE '$filename' decrypted (plain):" . PHP_EOL;
 $stream_read = fopen("crypto.file://" . $filename, "r", false, $context_read);
 if (!$stream_read) {
 	exit;
@@ -53,7 +64,12 @@ if (!$stream_read) {
 while ($data = fread($stream_read, 5)) {
 	echo $data;
 }
+echo file_get_contents("crypto.file://" . $filename, false, $context_read);
 echo PHP_EOL;
-//echo file_get_contents("crypto.file://" . $filename, false, $context_read);
 
-unlink($filename);
+
+// ---------------------------
+// DELETE the temporary file
+if (unlink($filename)) {
+	echo "FILE '$filename' deleted" . PHP_EOL;
+}
