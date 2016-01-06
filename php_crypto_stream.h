627ddac3081fb7f18f8a303d89d4e64ca52654b6
Jakub Zelenka <bukka@php.net>
2016-01-06 14:28:09 +0000

    Bump year

--- php_crypto_stream.h
+++ php_crypto_stream.h
@@ -2,7 +2,7 @@
   +----------------------------------------------------------------------+
   | PHP Version 5                                                        |
   +----------------------------------------------------------------------+
-  | Copyright (c) 2013-2015 Jakub Zelenka                                |
+  | Copyright (c) 2013-2016 Jakub Zelenka                                |
   +----------------------------------------------------------------------+
   | This source file is subject to version 3.01 of the PHP license,      |
   | that is bundled with this package in the file LICENSE, and is        |
