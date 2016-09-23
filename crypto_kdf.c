5edccab93ce1496dde567c70aee3e823df0c4759
Jakub Zelenka <bukka@php.net>
2016-09-23 19:48:11 +0100

    Add PKCS2 version test and KDF error definitions

--- crypto_kdf.c
+++ crypto_kdf.c
@@ -16,16 +16,24 @@
   +----------------------------------------------------------------------+
 */
 
-#include <openssl/evp.h>
-
-#if OPENSSL_VERSION_NUMBER >= 0x10000000L
-
 #include "php.h"
 #include "php_crypto.h"
 #include "zend_exceptions.h"
 #include "php_crypto_kdf.h"
 
+#include <openssl/evp.h>
 
-
-
+/* PKCS2 feature test */
+#if OPENSSL_VERSION_NUMBER >= 0x10000000L
+#define PHP_CRYPTO_HAS_PKCS2 1
 #endif
+
+PHP_CRYPTO_EXCEPTION_DEFINE(KDF)
+PHP_CRYPTO_ERROR_INFO_BEGIN(KDF)
+PHP_CRYPTO_ERROR_INFO_ENTRY(
+	DERIVATION_FAILED,
+	"KDF derivation failed"
+)
+PHP_CRYPTO_ERROR_INFO_END()
+
+
